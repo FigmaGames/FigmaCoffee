68718be289fb9c5b706868b8465a88d40e83aab1
Jorgepr02 <jorgepampinrios02@liceolapaz.net>
2025-03-16 13:28:33 +0100

    Mejorado nav añadido aria label a la hamburguesa, limpieza general del código, optimizado código y accesibilidad

--- js/index.js
+++ js/index.js
@@ -40,4 +40,48 @@
     });
     
     updateCarousel();
-  });
+
+    const mobileMenuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    if (mobileMenuButton && mobileMenu) {
+    mobileMenuButton.addEventListener('click', function() {
+        
+        mobileMenu.classList.toggle('open');
+        
+        if (mobileMenu.classList.contains('open')) {
+        mobileMenuButton.classList.add('text-[#D27640]');
+        } else {
+        mobileMenuButton.classList.remove('text-[#D27640]');
+        }
+    });
+    }
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        if (targetId === '#') return;
+        
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+        
+        if (mobileMenu && mobileMenu.classList.contains('open')) {
+            mobileMenu.classList.remove('open');
+            mobileMenuButton.classList.remove('text-[#D27640]');
+        }
+        const headerOffset = 80;
+        const elementPosition = targetElement.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+        
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+        });
+        }
+    });
+    });
+});
+
+  
