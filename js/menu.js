ab7bd7594f5413d10ff0505e1d38fb7f2dae167c
Fernando Rodríguez Rodríguez <26763307+Ferox13@users.noreply.github.com>
2025-03-19 10:51:22 +0100

    Correción header para mobil

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,16 @@
-document.getElementById('mobile-menu-button').addEventListener('click', function() {
-    const mobileMenu = document.getElementById('mobile-menu');
-    mobileMenu.classList.toggle('hidden');
+document.addEventListener('DOMContentLoaded', function() {
+  const mobileMenuButton = document.getElementById('mobile-menu-button');
+  const mobileMenu = document.getElementById('mobile-menu');
+  
+  mobileMenuButton.addEventListener('click', function() {
+    const expanded = mobileMenuButton.getAttribute('aria-expanded') === 'true';
+    
+    mobileMenuButton.setAttribute('aria-expanded', !expanded);
+    
+    if (expanded) {
+      mobileMenu.style.maxHeight = '0';
+    } else {
+      mobileMenu.style.maxHeight = mobileMenu.scrollHeight + 'px';
+    }
+  });
 });
